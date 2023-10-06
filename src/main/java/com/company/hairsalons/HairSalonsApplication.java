913ef55648e789dcbe8e60c35bf9b348d583abfa
magdalenahuget <magdalena.huget@gmail.com>
2023-10-06 20:48:43 +0200

    Add spring initialization

---
+++ src/main/java/com/company/hairsalons/HairSalonsApplication.java
@@ -0,0 +1,13 @@
+package com.company.hairsalons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairSalonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairSalonsApplication.class, args);
+	}
+
+}
